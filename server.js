98bffe2c69fcab7d2a33ba4cdfa621c5a8ad1f6a
Clarence Chan <38497249+clarencecodes@users.noreply.github.com>
2020-10-26 22:06:40 +0800

    Install morgan to log API requests in development mode

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 // Route files
 const prescriptions = require('./routes/prescriptions');
@@ -8,6 +9,11 @@ const prescriptions = require('./routes/prescriptions');
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/prescriptions', prescriptions);
