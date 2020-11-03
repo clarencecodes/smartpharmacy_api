c3f823e83b287f6541b22b76ed81b699b12bdac8
Clarence Chan <38497249+clarencecodes@users.noreply.github.com>
2020-11-03 14:44:04 +0800

    Implement dispensePrescription API

--- controllers/prescriptions.js
+++ controllers/prescriptions.js
@@ -1,3 +1,4 @@
+const Medicine = require('../models/Medicine');
 const Prescription = require('../models/Prescription');
 
 // @desc    Get all prescriptions
@@ -90,10 +91,73 @@ exports.deletePrescription = async (req, res, next) => {
 // @route   PUT /api/v1/prescriptions/dispense/:id
 // @access  Private
 exports.dispensePrescription = async (req, res, next) => {
+  var i;
   // Check if there is enough medicine to be dispensed and update stock levels
   // Set prescription dispenseStatus to 'dispensed'
   // Return the relevant drawers
-  res
-    .status(200)
-    .json({ success: true, msg: `prescription ${req.params.id} dispensed` });
+  const prescription = await Prescription.findById(req.params.id);
+
+  if (!prescription) {
+    return res.status(400).json({
+      success: false,
+      msg: `Unable to find prescription with id ${req.params.id}`,
+    });
+  }
+
+  if (prescription['dispenseStatus'] === 'dispensed') {
+    return res.status(400).json({
+      success: false,
+      msg: `Prescription ${req.params.id} has already been dispensed`,
+    });
+  }
+
+  const medicineIds = prescription['medicines'];
+
+  // Check if there are enough stocks
+  var medicinesArr = [];
+  for (i = 0; i < medicineIds.length; i++) {
+    let id = medicineIds[i];
+    let medicine = await Medicine.findById(id);
+    let updatedStock = medicine['stock'] - medicine['dosage'];
+
+    if (updatedStock < 0) {
+      return res.status(400).json({
+        success: false,
+        msg: `Not enough stock for medicine ${id}`,
+      });
+    }
+    medicinesArr.push(medicine);
+  }
+
+  // If there are enough stocks for all medicines, update the DB
+  // Dispense the medicines
+  for (i = 0; i < medicinesArr.length; i++) {
+    let medicine = medicinesArr[i];
+    await medicine.update(
+      {
+        stock: medicine['stock'] - medicine['dosage'],
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+  }
+
+  // Set the prescription status to 'dispensed'
+  await prescription.update(
+    {
+      dispenseStatus: 'dispensed',
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).json({
+    success: true,
+    msg: `Prescription ${req.params.id} dispensed`,
+    data: medicinesArr,
+  });
 };
