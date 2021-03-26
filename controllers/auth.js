ba8cae3cf7a5030ba8182cac972ad9227cba8d83
Clarence Chan <38497249+clarencecodes@users.noreply.github.com>
2021-03-26 21:14:19 +0800

    add try-catch block to updateDetails and updatePassword API

--- controllers/auth.js
+++ controllers/auth.js
@@ -82,39 +82,47 @@ exports.getMe = async (req, res, next) => {
 // @route       PUT /api/v1/auth/updateDetails
 // @access      Private
 exports.updateDetails = async (req, res, next) => {
-  const fieldsToUpdate = {
-    name: req.body.name,
-    email: req.body.email,
-  };
+  try {
+    const fieldsToUpdate = {
+      name: req.body.name,
+      email: req.body.email,
+    };
 
-  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
-    new: true,
-    runValidators: true,
-  });
+    const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+      new: true,
+      runValidators: true,
+    });
 
-  res.status(200).json({
-    success: true,
-    data: user,
-  });
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (err) {
+    res.status(400).json({ success: false, msg: err.toString() });
+  }
 };
 
 // @desc        Update password
 // @route       PUT /api/v1/auth/updatePassword
 // @access      Private
 exports.updatePassword = async (req, res, next) => {
-  const user = await User.findById(req.user.id).select('+password');
+  try {
+    const user = await User.findById(req.user.id).select('+password');
 
-  // Check current password
-  if (!(await user.matchPassword(req.body.currentPassword))) {
-    return res
-      .status(401)
-      .json({ success: false, msg: 'Password is incorrect' });
+    // Check current password
+    if (!(await user.matchPassword(req.body.currentPassword))) {
+      return res
+        .status(401)
+        .json({ success: false, msg: 'Password is incorrect' });
+    }
+
+    user.password = req.body.newPassword;
+    await user.save();
+
+    sendTokenResponse(user, 200, res);
+  } catch (err) {
+    res.status(400).json({ success: false, msg: err.toString() });
   }
-
-  user.password = req.body.newPassword;
-  await user.save();
-
-  sendTokenResponse(user, 200, res);
 };
 
 // Get token from model, create cookie and send response
